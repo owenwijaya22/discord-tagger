8dfdabb0f2ef6a52a089f9e60fe339a371337e5e
unknown <owenwijaya89@gmail.com>
2022-01-04 15:40:08 +0700

    added the files necessary for the project

---
+++ src/tagger.py
@@ -0,0 +1,37 @@
+import requests
+import random
+import json
+
+
+def choose_token():
+    token = "OTI1MjM0NjY4MDc5NTcwOTQ0.YcqKIg.oR-TjoCwci2N_gMekjkWSpS3he4"
+    #token = random.choice(["OTI1MjM0NjY4MDc5NTcwOTQ0.YcqKIg.oR-TjoCwci2N_gMekjkWSpS3he4"])
+    return token
+
+
+def choose_url():
+    subject = input('Math (m) or Physics (p): ')
+    if subject == 'm':
+        channels = random.choice([
+            "536995777981972491", "754860723321962628", "641351291343208448",
+            "917170713755017217", "803057978277888020", "704944645712642098"
+        ])
+        url = f"https://discord.com/api/v9/channels/{channels}/messages"
+    elif subject == 'p':
+        channels = random.choice(["627265780056195083", "536995799859724309"])
+        url = f"https://discord.com/api/v9/channels/{channels}/messages"
+    return url, channels
+
+
+def main():
+    url, channels = choose_url()
+    token = choose_token()
+    payload = "{\"content\":\"<@!300506208832585728> thank you\",\"nonce\":\"923027088506617856\",\"tts\":false}"
+    headers = {'authorization': token, 'content-type': "application/json"}
+    response = requests.post(url, data=payload,headers=headers).json()
+    message_id = response['id']
+    delete_url = f"https://discord.com/api/v9/channels/{channels}/messages/{message_id}"
+    requests.delete(delete_url, headers=headers)
+
+
+main()
